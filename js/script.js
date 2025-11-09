4defaa721d779fc4807e35a93b37d674f8c7d571
Ezequiel Sanchez <sancheze0114@gmail.com>
2025-11-08 21:26:41 -0300

    archivos con carrito

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // VARIABLES: ES UN ESPACIO DE MEMORIA RESERVADO PARA ALMACENAR INFORMACION IMPORTANTE DEL PROGRAMA //
 
 // DECLARACION:
- //let cliente;
+//let cliente;
 
 // ASIGNACION DE VALORES:
 //cliente = "Cynthia" 
@@ -18,3 +18,31 @@
 //const nacimiento = 1990
 
 //console.log (nacimiento)
+
+document.addEventListener("DOMContentLoaded", function () {
+    const boton = document.getElementById("botonWhatsapp");
+
+    boton.addEventListener("click", function (event) {
+        event.preventDefault(); // Evita que el link se abra sin confirmación
+
+        const confirmacion = confirm("¿Deseás salir y abrir WhatsApp?");
+
+        if (confirmacion) {
+            window.open("https://wa.me/5491176444703", "_blank");
+        } else {
+            alert("Perfecto, Segui comprando en SDY Herramientas.");
+        }
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  const botonesCarrito = document.querySelectorAll(".btn-carrito");
+
+  botonesCarrito.forEach(boton => {
+    boton.addEventListener("click", function() {
+      alert("✅ Producto agregado al carrito");
+      window.location.href = "carrito.html";
+    });
+  });
+});
+
